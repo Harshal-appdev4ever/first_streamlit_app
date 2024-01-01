44bb35eeabb9f498ae6b229814ebc1ba7dab6066
Harshal-appdev4ever <155362878+Harshal-appdev4ever@users.noreply.github.com>
2024-01-01 19:05:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healty dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text('Kale, Spinach and Rocket')
+streamlit.text('Vegetables')
